480ba58afa1d9420f25d53fb5ff618d68302747d
Widya Septiawan <widya.septiawan@stickearn.com>
2024-05-03 10:22:10 +0700

    Adjust init database config

--- src/common/database/mongo/mongo.module.ts
+++ src/common/database/mongo/mongo.module.ts
@@ -2,28 +2,17 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { MongoService } from "./mongo.service";
 import { ConfigService } from "@nestjs/config";
+import { UserSchema } from "src/entity/model/user.model";
+import mongoose from "mongoose";
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        uri: (new MongoService(configService)).uri(),
-        connectionFactory: connection => {
-          connection.on("connected", () => {
-            console.log("~ DB connected");
-          });
-          connection.on("disconnected", () => {
-            console.log("~ DB disconnected");
-          });
-          connection.on("error", error => {
-            console.log("~ DB connection failed! for error: ", error);
-          });
-          connection._events.connected();
-          return connection;
-        },
-      }),
+      useClass: MongoService,
       inject: [ConfigService],
     })
   ],
+  providers: [MongoService],
+  exports: [MongoService]
 })
 export class MongoModule {}
--- src/common/database/mongo/mongo.service.ts
+++ src/common/database/mongo/mongo.service.ts
@@ -1,13 +1,37 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { MongooseModuleOptions, MongooseOptionsFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
 import { DatabaseConfig } from "src/common/config/configuration";
 
 @Injectable()
-export class MongoService {
-  constructor(private configService?: ConfigService) {}
+export class MongoService implements MongooseOptionsFactory {
+  constructor(@Inject(ConfigService) private readonly configService?: ConfigService) {
+    this.configService = configService;
+  }
 
   uri(): string {
     const database = this.configService.get<DatabaseConfig>('database.mongo');
-    return `mongodb://${database.user}:${database.pass}@${database.host}:${database.port}/${database.name}?authSource=${database.auth}`; 
-  }   
+    return `mongodb://${database.user}:${database.pass}@${database.host}:${database.port}/${database.name}?authSource=${database.auth}`;
+  }
+
+  createMongooseOptions(): MongooseModuleOptions | Promise<MongooseModuleOptions> {
+    return {
+      uri: this.uri(),
+      connectionFactory: async (connection) => {
+        await mongoose.connect(this.uri())
+        connection.on("connected", () => {
+          console.log("~ DB connected");
+        });
+        connection.on("disconnected", () => {
+          console.log("~ DB disconnected");
+        });
+        connection.on("error", error => {
+          console.log("~ DB connection failed! for error: ", error);
+        });
+        connection._events.connected();
+        return connection;
+      }
+    }
+  }
 }
